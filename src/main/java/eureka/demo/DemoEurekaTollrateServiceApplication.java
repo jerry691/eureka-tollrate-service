187f42aa1fe901835637565bac0d82529b4a3bcf
jerry691 <jerrywang6886@gmail.com>
2020-07-20 10:03:40 -0400

    initial commit

---
+++ src/main/java/eureka/demo/DemoEurekaTollrateServiceApplication.java
@@ -0,0 +1,12 @@
+package eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaTollrateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaTollrateServiceApplication.class, args);
+	}
+}
